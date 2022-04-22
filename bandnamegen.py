59d949e619b8be2e8762f16a7d5af0f17e754ea7
loading.chasm <44106397+loadingchasm@users.noreply.github.com>
2022-04-22 15:19:08 -0400

    Create bandnamegen.py

---
+++ bandnamegen.py
@@ -0,0 +1,5 @@
+print("Hello and welcome to the Band Name Generator v1.0.")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+band_name = city + " " + pet
+print("Your band name could be: " + band_name)
