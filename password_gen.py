061cdf4c9c4ff3d2416ad4d43a03cfc05ef1ed30
loading.chasm <44106397+loadingchasm@users.noreply.github.com>
2022-04-22 15:17:05 -0400

    Create password_gen.py

---
+++ password_gen.py
@@ -0,0 +1,29 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+for letter in range(0, nr_letters):
+  numLetter = random.randint(0, 52)
+  password += letters[numLetter - 1]
+
+for symbol in range(0, nr_symbols):
+  numSymbol = random.randint(0, 9)
+  password += symbols[numSymbol - 1]
+
+for number in range(0, nr_numbers):
+  numInt = random.randint(0, 10)
+  password += numbers[numInt - 1]
+
+splitPass = list(password)
+random.shuffle(splitPass)
+newPass = "".join(splitPass)
+
+print(f"Your password is: {newPass}")
